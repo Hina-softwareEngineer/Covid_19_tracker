80ba062537ae5d130d5fee2234884720128800dc
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-30 21:10:31 +0500

    last updated

--- src/components/apichecker.js
+++ src/components/apichecker.js
@@ -25,15 +25,25 @@ class ApiChecker extends Component {
     }
 
     render() {
-        let data = this.state;
-        console.log(data.data)
+        let results = this.state;
+        console.log(results.data)
 
         return (
             <div>
                 <h1>Hina</h1>
-                <h1>Confirmed : {data.confirmed}</h1>
-                <h1>Deaths : {data.deaths}</h1>
-                <h1>Recovered : {data.recovered}</h1>
+                <h1>Confirmed : {results.confirmed}</h1>
+                <h1>Deaths : {results.deaths}</h1>
+                <h1>Recovered : {results.recovered}</h1>
+
+                {
+                    (Object.keys(results.data).length > 1) ?
+                        <div><p>Confirmed Last Updated : {results.data.confirmed.last_updated}</p>
+                            <p>Confirmed Last Updated : {results.data.deaths.last_updated}</p>
+                            <p>Confirmed Last Updated : {results.data.recovered.last_updated}</p>
+                        </div>
+                        : null
+                }
+
 
             </div>
         );
